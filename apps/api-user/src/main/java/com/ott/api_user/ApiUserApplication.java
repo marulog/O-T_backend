91201f8ac7cbffd128b103652ced4aa60e3425d2
marulog <gr214777@naver.com>
2026-06-25 20:26:04 +0900

    [INIT]: redis 설정

--- apps/api-user/src/main/java/com/ott/api_user/ApiUserApplication.java
+++ apps/api-user/src/main/java/com/ott/api_user/ApiUserApplication.java
@@ -3,6 +3,7 @@ package com.ott.api_user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 @ComponentScan(basePackages = "com.ott")
 @EntityScan(basePackages = "com.ott.domain")
 @EnableJpaRepositories(basePackages = "com.ott.domain")
@@ -20,7 +22,7 @@ public class ApiUserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiUserApplication.class, args);
-		
+
 	}
 
 }
